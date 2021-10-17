ffa99bbe7244f00cb7a102d4a410c3579edc628e
LeeSeungKeun <ksks936@gmail.com>
2021-10-17 21:53:03 +0900

    first commit

---
+++ SocialLoginTest/VC/MyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyViewController.swift
+//  SocialLoginTest
+//
+//  Created by 이성근 on 2021/10/17.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+}
---
+++ SocialLoginTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLoginTest
+//
+//  Created by 이성근 on 2021/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
